32b9967d516cd53b4a11d225b3aaa7549744b303
105927405 <105927405@student.swin.edu.au>
2026-05-28 21:00:30 +1000

    Index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<!--------------------------------------------------
+//                                                //
+//          Blade EduNET Index Page PHP           //
+//          Author: Blake Stone                   //
+//          Student ID: 105927405                 //
+//          Date of creation: 24/5/2026           //
+//          File Size: 4622 bytes                 //
+//                                                //
+--------------------------------------------------->
+
+<!DOCTYPE html>
+<html lang = "en">
+<?php include 'header.inc'; ?>
+
+
+        <div class = "WelcomeContainer">
+            <h3>Welcome to Blade EduNET!</h3>
+            <p>Blade EduNET is an Education Technology company which focuses on developing digital learning tools and platforms.</p>
+            
+            <p>We specialise in educational platforms to help our younger generation learn and grow in school environments.</p>
+        
+        </div>
+
+
+        <div class = "TextContainer">
+            <h3>Acknowledgement of Country</h3>
+            <p> <strong>BladeNET </strong> would like to acknowledge the Traditional Custodians of the land on which their work is created and offices reside, 
+                We pay our respects to their Elders past, present, and emerging. And recognise their continuing connection to land, waters, and culture, 
+                and acknowledge that sovereignty was never ceded. We extend this respect to all Aboriginal and Torres Strait Islander peoples.
+            </p>
+        </div>
+
+        <div class = "TextContainer">
+            <h3>Company Release Schedule</h3>
+            <p>Please find our upcoming release and update schedule for 2026 below.</p>
+
+            <table>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Release Type</th>
+                    <th>Version Num</th>
+                    <th>Release Quarter</th>
+                </tr>
+                <tr>
+                    <td>Blade EduMATH</td>
+                    <td>Minor Update</td>
+                    <td>V1.0.8</td>
+                    <td>Q3 2026</td>
+                </tr>
+                <tr>
+                    <td>Blade EduLIT</td>
+                    <td>Minor Update</td>
+                    <td>V1.0.8</td>
+                    <td>Q3 2026</td>
+                </tr>
+                <tr>
+                    <td>Blade EduADMIN</td>
+                    <td>Major Update</td>
+                    <td>V1.2.0</td>
+                    <td>Q4 2026</td>
+                </tr>
+                <tr>
+                    <td>Blade EduSTREAM</td>
+                    <td>New</td>
+                    <td colspan = "2" rowspan = "2">TBA</td>
+                </tr>
+                <tr>
+                    <td>Blade EduSTEM</td>
+                    <td>New</td>
+                </tr>
+            </table>
+        </div>
+
+     <?php include 'footer.inc'; ?>
+</body>
+</html>
