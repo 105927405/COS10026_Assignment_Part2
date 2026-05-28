42aa5f77ccb20abd4bf4fd30059b4e94f236b25a
105927405 <105927405@student.swin.edu.au>
2026-05-28 21:03:18 +1000

    Updated header.inc and software.php

---
+++ software.php
@@ -0,0 +1,137 @@
+<!--------------------------------------------------
+//                                                //
+//          Blade EduNET Software Page            //
+//          Author: Blake Stone                   //
+//          Student ID: 105927405                 //
+//          Date of creation: 18/4/2026           //
+//          File Size: 6928 bytes                 //
+//                                                //
+--------------------------------------------------->
+
+<!DOCTYPE html>
+<html lang = "en">
+<?php include 'header.inc'; ?>
+
+     <div class = "JobContainer"> <!--Reusing job container css item as it's easier then just making a new one and does what i need it to do-->
+    <article>
+        <h2> Available Software </h2>
+        <p> <strong>Blade EduNET</strong> is an Educational Technology company which focuses on developing digital learning tools and platforms.
+             We offer various online solutions for educational learning and development, ranging from classroom learning tools, to school Mannagement solutions.
+             If you are interested in deploying our available solutions in your school, please contact us! <br>
+             Please find our currently avalible software packages are listed below. 
+        </p>
+    </article>
+    <br>
+</div>
+
+<div class = "JobContainer">
+    <section>
+            <h3> Blade EduMATH </h3>
+
+            <aside> 
+                <ul> 
+                    <li> Cost 
+                        <ul> 
+                            <li> Annual License: $10 Per Student </li>
+                        </ul>
+                    </li>
+                    <li> Version 
+                        <ul> 
+                            <li> V1.0.7 </li>
+                        </ul>
+                    </li>
+                </ul>
+                <a href="soon.html">More Info</a>
+            </aside>
+            <h4> About this product </h4>
+            <p> Blade EduMATH is a web-based platform designed to support mathematics learning through online tests, quizzes, and interactive activities.
+                It enables teachers to create, assign, and automatically assess student work in real time.
+                The tool helps track student progress and improves engagement in math education. 
+            </p>
+            <h5> Features: </h5>
+            <ol>
+                <li> Student Testing </li>
+                <li> F-y12 Math Levels </li>
+                <li> Math quizs </li>
+                <li> And More! </li>
+            </ol>
+ 
+    </div>
+</section>
+
+    <div class = "JobContainer">
+        <section>
+            <h3> Blade EduLIT </h3>
+
+            <aside> 
+                <ul> 
+                    <li> Cost 
+                        <ul> 
+                            <li> Annual License: $10 Per Student </li>
+                        </ul>
+                    </li>
+                    <li> Version 
+                        <ul> 
+                            <li> V1.0.7 </li>
+                        </ul>
+                    </li>
+                </ul>
+                <a href="soon.html">More Info</a>
+            </aside>
+            <h4> About this product </h4>
+            <p> Blade EduLIT is a web-based learning tool focused on literature education, offering quizzes, assessments, and reading-based activities.
+                Teachers can manage tasks, evaluate student responses, and support literacy development online.
+                It promotes critical thinking and comprehension through structured digital learning experiences.
+            </p>
+            <h5> Features: </h5>
+            <ol>
+                <li> Student Testing </li>
+                <li> y7-y12 Literature Levels </li>
+                <li> Writing Tasks </li>
+                <li> And More! </li>
+            </ol>
+
+    </div>
+</section>
+
+<div class = "JobContainer">
+        <section>
+            <h3> Blade EduADMIN </h3>
+
+            <aside> 
+                <ul> 
+                    <li> Cost 
+                        <ul> 
+                            <li> Annual License: $10,000 </li>
+                        </ul>
+                    </li>
+                    <li> Version 
+                        <ul> 
+                            <li> V1.1.12 </li>
+                        </ul>
+                    </li>
+                </ul>
+                <a href="soon.html">More Info</a>
+            </aside>
+            <h4> About this product </h4>
+            <p> Blade EduADMIN is a web-based school administration system that streamlines tasks such as student records, scheduling, and communication.
+                It centralises administrative processes into one platform, improving efficiency and organisation.
+                Similar systems help automate enrolments, reporting, and overall school management workflows 
+            </p>
+            <h5> Features: </h5>
+            <ol>
+                <li> Classroom Roll Taking </li>
+                <li> School Notice Board </li>
+                <li> Parent Messaging </li>
+                <li> And More! </li>
+            </ol>
+           
+    </div>
+</section>
+
+
+
+    </div>
+     <?php include 'footer.inc'; ?>
+</body>
+</html>
